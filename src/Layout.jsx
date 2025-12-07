69b826db11c33cbe95a760ebc12d74fc58ab0965
Ago Chinedum <chinedumago@gmail.com>
2025-12-07 13:12:26 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -64,7 +64,7 @@ export default function Layout({ children, currentPageName }) {
   };
 
   const handleLogout = () => {
-    base44.auth.logout();
+    base44.auth.logout(createPageUrl('Home'));
   };
 
   const navItems = [
